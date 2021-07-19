88bc9ecadbf9d914328c3f8a12ceda526bf3fc6f
atjohnfeng <atjohnfeng@gmail.com>
2021-07-19 16:48:06 -0700

    added my_each

--- W3/W3D1/enumerables/enumerables.rb
+++ W3/W3D1/enumerables/enumerables.rb
@@ -1,1 +1,27 @@
-p "hello"
+class Array
+
+    def my_each(&prc)
+      i = 0
+      while i < self.length
+          prc.call(self[i])
+          i+=1
+      end
+      self
+    end
+
+end
+
+ # calls my_each twice on the array, printing all the numbers twice.
+return_value = [1, 2, 3].my_each do |num|
+  puts num
+end.my_each do |num|
+  puts num
+end
+# => 1
+    #  2
+    #  3
+    #  1
+    #  2
+    #  3
+
+p return_value  # => [1, 2, 3]
