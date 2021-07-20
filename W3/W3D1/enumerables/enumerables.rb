0a51d0cbd58fb1b8e2dc00d8f2afa404f13a248f
your name here <your_email_hereroelliga@yahoo.com>
2021-07-19 17:06:42 -0700

    added my_reject and my_select

--- W3/W3D1/enumerables/enumerables.rb
+++ W3/W3D1/enumerables/enumerables.rb
@@ -9,19 +9,60 @@ class Array
       self
     end
 
+def my_select(&prc)
+  new_arr = []
+  self.my_each(&prc)
+  i = 0
+  while i < self.length
+    if prc.call(self[i])
+      new_arr << self[i]
+    end
+    i += 1
+  end
+  new_arr
 end
 
- # calls my_each twice on the array, printing all the numbers twice.
-return_value = [1, 2, 3].my_each do |num|
-  puts num
-end.my_each do |num|
-  puts num
-end
-# => 1
-    #  2
-    #  3
-    #  1
-    #  2
-    #  3
 
-p return_value  # => [1, 2, 3]
+def my_reject(&prc)
+  new_arr = []
+  # self.my_each(&prc)
+  i = 0
+  while i < self.length
+    if !prc.call(self[i])
+      new_arr << self[i]
+    end
+    i += 1
+  end
+  new_arr
+end
+
+# def my_any(&prc)
+#   i = 0
+#   while i < self.length
+#     if prc.call(self[i])
+#       return true
+end
+
+p a = [1, 2, 3]
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+# p a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+
+#  # calls my_each twice on the array, printing all the numbers twice.
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+# end.my_each do |num|
+#   puts num
+# end
+# # => 1
+#      2
+#      3
+#      1
+#      2
+#      3
+
+# p return_value  # => [1, 2, 3]
+
