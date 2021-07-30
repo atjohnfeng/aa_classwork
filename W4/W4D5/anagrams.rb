938abf38babaf646cb1f33b875dea3f8bcdcbcf2
atjohnfeng <atjohnfeng@gmail.com>
2021-07-30 16:02:47 -0700

    Completed Anagrams

--- W4/W4D5/anagrams.rb
+++ W4/W4D5/anagrams.rb
@@ -1,9 +1,9 @@
-def first_anagram?(str1, str2)
-  anagrams = str1.split('').permutation.to_a #n!
+def first_anagram?(str1, str2) #n!
+  anagrams = str1.split('').permutation.to_a 
   anagrams.include?(str2.split(''))
 end
 
-def second_anagram?(str1, str2)
+def second_anagram?(str1, str2) #n^2
   second_word = str2.split('')
   str1.split('').each do |c|
     idx = second_word.find_index(c)
@@ -15,3 +15,20 @@ def second_anagram?(str1, str2)
   end
   second_word.empty?
 end
+
+def third_anagram?(str1, str2) #n log n ? n^2 ?
+    first = str1.split("").sort
+    second = str2.split("").sort
+    first == second
+end
+
+def fourth_anagram?(str1,str2) #n
+    hash1 = Hash.new(0) 
+    hash2 = Hash.new(0)
+    str1.each_char { |c| hash1[c] += 1 }
+    str2.each_char { |c| hash2[c] += 1 }
+    hash1 == hash2
+end
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
