a0d05c182d6c657ab3af6bdd84ed4f4322be96dd
atjohnfeng <atjohnfeng@gmail.com>
2021-07-30 14:39:13 -0700

    Completed execution_time

--- W4/W4D5/execution_time.rb
+++ W4/W4D5/execution_time.rb
@@ -54,17 +54,24 @@ def largest_contiguous_subsum(arr)
     largest_sum = arr.first
     curr_sum = arr.first
     (1...arr.length).each do |i|
-      if curr_sum + arr[i] > 0  
-        curr_sum += arr[i]
-        if curr_sum > largest_sum 
-          largest_sum = curr_sum
-        end 
-        else curr_sum + arr[i] < 0
+        if curr_sum + arr[i] > 0  
+            curr_sum += arr[i]
+            if curr_sum > largest_sum 
+                largest_sum = curr_sum
+            end 
+        elsif curr_sum + arr[i] < 0
+            if arr[i] > largest_sum 
+            largest_sum = arr[i]
+            end 
             curr_sum = 0
-      end
+        end
     end
     largest_sum
 end
 
 list = [2, 3, -6, 7, -6, 7]
+
 p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+# list = [-5, -1, -3]
+# p largest_contiguous_subsum(list) # => -1 (from [-1])
