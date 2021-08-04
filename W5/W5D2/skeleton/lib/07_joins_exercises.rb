57e76c855a1b1fc77057245ef52a5eb0f97382ca
atjohnfeng <atjohnfeng@gmail.com>
2021-08-03 17:29:23 -0700

    Joins exercises

--- W5/W5D2/skeleton/lib/07_joins_exercises.rb
+++ W5/W5D2/skeleton/lib/07_joins_exercises.rb
@@ -40,6 +40,16 @@ end
 def ford_films
   # List the films in which 'Harrison Ford' has appeared.
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    JOIN castings
+      ON movies.id = castings.movie_id
+    JOIN actors
+      ON castings.actor_id = actors.id
+    WHERE
+      name = 'Harrison Ford'
   SQL
 end
 
@@ -48,12 +58,32 @@ def ford_supporting_films
   # role. [Note: the ord field of casting gives the position of the actor. If
   # ord=1 then this actor is in the starring role]
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    JOIN castings
+      ON movies.id = castings.movie_id
+    JOIN actors
+      ON castings.actor_id = actors.id
+    WHERE
+      name = 'Harrison Ford' AND ord != 1
   SQL
 end
 
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+    SELECT
+      title, name
+    FROM
+      movies
+    JOIN castings
+      ON movies.id = castings.movie_id
+    JOIN actors
+      ON castings.actor_id = actors.id
+    WHERE
+      yr = 1962 AND ord = 1
   SQL
 end
 
@@ -61,6 +91,20 @@ def travoltas_busiest_years
   # Which were the busiest years for 'John Travolta'? Show the year and the
   # number of movies he made for any year in which he made at least 2 movies.
   execute(<<-SQL)
+    SELECT
+      yr, COUNT(*) as num_movies
+    FROM
+      movies
+    JOIN castings
+      ON movies.id = castings.movie_id
+    JOIN actors
+      ON castings.actor_id = actors.id
+    WHERE
+      name = 'John Travolta'
+    GROUP BY
+      yr
+    HAVING
+      COUNT(*) >= 2
   SQL
 end
 
@@ -68,6 +112,27 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+    SELECT
+        title, name
+      FROM
+        movies
+      JOIN castings
+        ON movies.id = castings.movie_id
+      JOIN actors
+        ON castings.actor_id = actors.id
+      WHERE
+        movie_id IN(
+          SELECT
+            movie_id
+          FROM
+            movies
+          JOIN castings
+            ON movies.id = castings.movie_id
+          JOIN actors
+            ON castings.actor_id = actors.id
+          WHERE
+            name = 'Julie Andrews'
+        ) AND ord = 1
   SQL
 end
 
