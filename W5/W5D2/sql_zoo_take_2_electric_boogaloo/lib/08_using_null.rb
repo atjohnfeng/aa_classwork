c8a0081c3b6fd4af5c8d72d50a551f7a0c6c0acd
atjohnfeng <atjohnfeng@gmail.com>
2021-08-07 18:20:10 -0700

    Completed some null Exercices

--- W5/W5D2/sql_zoo_take_2_electric_boogaloo/lib/08_using_null.rb
+++ W5/W5D2/sql_zoo_take_2_electric_boogaloo/lib/08_using_null.rb
@@ -18,6 +18,12 @@ require_relative './sqlzoo.rb'
 def null_dept
   # List the teachers who have NULL for their department.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      teachers
+    WHERE
+      dept_id IS NULL
   SQL
 end
 
@@ -25,6 +31,14 @@ def all_teachers_join
   # Use a type of JOIN that will list all teachers and their department,
   # even if the department in NULL/nil.
   execute(<<-SQL)
+    SELECT
+      teachers.name, depts.name
+    FROM
+      teachers
+    LEFT JOIN
+      depts
+    ON
+      dept_id = depts.id
   SQL
 end
 
@@ -33,6 +47,14 @@ def all_depts_join
   # NB: you can avoid RIGHT OUTER JOIN (and just use LEFT) by swapping
   # the FROM and JOIN tables.
   execute(<<-SQL)
+    SELECT
+      teachers.name, depts.name
+    FROM
+      depts
+    LEFT JOIN
+      teachers
+    ON
+      teachers.dept_id = depts.id
   SQL
 end
 
@@ -41,6 +63,10 @@ def teachers_and_mobiles
   # 444 2266' if no number is given. Show teacher name and mobile
   # #number or '07986 444 2266'
   execute(<<-SQL)
+    SELECT
+      teachers.name, COALESCE(teachers.mobile, '07986 444 2266')
+    FROM
+      teachers
   SQL
 end
 
@@ -49,6 +75,14 @@ def teachers_and_depts
   # department name. Use the string 'None' where there is no
   # department.
   execute(<<-SQL)
+    SELECT
+      teachers.name, COALESCE(depts.name, 'None')
+    FROM
+      teachers
+    LEFT JOIN
+      depts
+    ON
+      teachers.dept_id = depts.id
   SQL
 end
 
@@ -57,6 +91,10 @@ def num_teachers_and_mobiles
   # mobile phones.
   # NB: COUNT only counts non-NULL values.
   execute(<<-SQL)
+    SELECT
+      COUNT(name), COUNT(mobile)
+    FROM
+      teachers
   SQL
 end
 
@@ -65,6 +103,16 @@ def dept_staff_counts
   # the number of staff. Structure your JOIN to ensure that the
   # Engineering department is listed.
   execute(<<-SQL)
+    SELECT
+      depts.name, COUNT(teachers.name)
+    FROM
+      depts
+    LEFT JOIN
+      teachers
+    ON
+      depts.id = teachers.dept_id
+    GROUP BY
+      depts.name
   SQL
 end
 
