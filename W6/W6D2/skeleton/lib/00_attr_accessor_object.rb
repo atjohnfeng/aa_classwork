9b256be56f80f7e82cb1cc11800df80166697466
atjohnfeng <atjohnfeng@gmail.com>
2021-08-10 15:34:51 -0700

    Added getter and setter methods

--- W6/W6D2/skeleton/lib/00_attr_accessor_object.rb
+++ W6/W6D2/skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,20 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    
+    names.each do |name|
+      #getter
+      define_method(name) do
+        return @name
+      end
+
+      #setter
+      setter = (name.to_s + '=')
+      define_method(setter) do
+        |new_ele|
+        @name = new_ele
+      end
+    end
+
+   
   end
 end
