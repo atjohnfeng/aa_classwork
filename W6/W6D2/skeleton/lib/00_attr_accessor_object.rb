fae0751219613481bf2d29b9d86ebdd759e11ad2
atjohnfeng <atjohnfeng@gmail.com>
2021-08-10 16:47:27 -0700

    Completed Attr_accessor

--- W6/W6D2/skeleton/lib/00_attr_accessor_object.rb
+++ W6/W6D2/skeleton/lib/00_attr_accessor_object.rb
@@ -4,17 +4,16 @@ class AttrAccessorObject
     names.each do |name|
       #getter
       define_method(name) do
-        return @name
+        instance_variable_get("@#{name}")
       end
 
       #setter
       setter = (name.to_s + '=')
       define_method(setter) do
         |new_ele|
-        @name = new_ele
+        instance_variable_set("@#{name}", new_ele)
       end
-    end
 
-   
+    end
   end
 end
