1d13abe09db28c578a32bdded5fb14b8d515f82c
Jason <k.jason0105@gmail.com>
2021-08-11 16:58:17 -0700

    artwork/artworkShares

--- W6/W6D3/first_routes_and_controllers/app/models/user.rb
+++ W6/W6D3/first_routes_and_controllers/app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
     foreign_key: :artist_id,
     class_name: :Artwork
 
+
+    has_many :viewed_artworks,
+    primary_key: :id,
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare
+
+    has_many :shared_artworks,
+    through: :viewed_artworks,
+    source: :artwork
 end
--- W6/W6D3/first_routes_and_controllers/db/seeds.rb
+++ W6/W6D3/first_routes_and_controllers/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+
+  User.destroy_all
+
+  Artwork.destroy_all
+
+  ArtworkShare.destroy_all
+
+
+end
