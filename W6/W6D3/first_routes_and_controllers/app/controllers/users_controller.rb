0011d337f048b7e88391e19a6c9e475a910f7542
atjohnfeng <atjohnfeng@gmail.com>
2021-08-11 15:02:55 -0700

    Added create and show methods

--- W6/W6D3/first_routes_and_controllers/app/controllers/users_controller.rb
+++ W6/W6D3/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def create
+    render json: params
+  end
+
+  def show
+    render json: params
+  end
+
   def index
     render plain: "I'm in the index action!"
   end
