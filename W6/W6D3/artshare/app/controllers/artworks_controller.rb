eef22dbdf59ab4e162708daf533abc57f2376ba9
Jason <k.jason0105@gmail.com>
2021-08-12 11:38:04 -0700

    added artworksController

--- W6/W6D3/artshare/app/controllers/artworks_controller.rb
+++ W6/W6D3/artshare/app/controllers/artworks_controller.rb
@@ -9,7 +9,52 @@ class ArtworksController < ApplicationController
         end
     end
 
-    
+    def index
+        @artworks = Artwork.all
+        render json: @artworks
+    end
+
+    def show
+        @artwork = Artwork.find_by(id: params[:id])
+        if @artwork
+            render json: @artwork
+        else
+            render json: {error: 'There is no artwork with that id.'}, status: 404
+        end
+    end
+
+    # def create
+    #     @artwork = Artwork.new(artwork_params)
+    #     if @artwork.save
+    #         redirect_to artwork_url(@artwork)
+    #     else
+    #         render json: @artwork.errors.full_messages, statuquit2
+    #     end
+    # end
+
+    def update
+        @artwork = Artwork.find_by(id: params[:id])
+        if @artwork.nil?
+            render json: {error: 'There is no artwork with that id.'}, status: 404
+            return
+        end
+        if @artwork.update(artwork_params)
+            redirect_to artwork_url(@artwork)
+        else
+            render json: @artwork.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @artwork = Artwork.find_by(id: params[:id])
+        if @artwork
+            @artwork.destroy
+            render json: @artwork
+        else
+            render json: {error: 'There is no artwork with that id.'}, status: 404
+        end
+    end
+
 
     private
 
