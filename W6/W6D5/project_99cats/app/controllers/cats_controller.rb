e15b60d59a438a1d848e398bb3cc2d31a5cf75f9
atjohnfeng <atjohnfeng@gmail.com>
2021-08-13 14:28:46 -0700

    Added new and create methods for cats

--- W6/W6D5/project_99cats/app/controllers/cats_controller.rb
+++ W6/W6D5/project_99cats/app/controllers/cats_controller.rb
@@ -15,4 +15,22 @@ class CatsController < ApplicationController
         end
     end
 
+    def new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
+    end
+
 end
