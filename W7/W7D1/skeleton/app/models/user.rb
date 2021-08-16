20085256cc6fa5574ee4866e93e9f7ccde6e52c5
atjohnfeng <atjohnfeng@gmail.com>
2021-08-16 14:04:58 -0700

    Added user class and model

---
+++ W7/W7D1/skeleton/app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+    validates :user_name, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :session_token, uniqueness: { scope: :user_name }
+
+    after_initialize :ensure_session_token
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save
+        self.session_token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+end
