67889b55990cefba9394a64e53345b322ec6687b
atjohnfeng <atjohnfeng@gmail.com>
2021-08-17 16:12:02 -0700

    Add UsersController methods

---
+++ W7/W7D2/MusicApp/app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            log_in(user)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        user.destroy if user
+        redirect_to new_session_url
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email)
+    end
+
+end
