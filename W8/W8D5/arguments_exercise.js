f844090e5a02d4c7cc4a4583c31e1640451a915f
atjohnfeng <atjohnfeng@gmail.com>
2021-08-27 15:37:23 -0700

    Test MyBind

--- W8/W8D5/arguments_exercise.js
+++ W8/W8D5/arguments_exercise.js
@@ -27,5 +27,50 @@ Function.prototype.myBind = function(context) {
 }
 
 Function.prototype.myBindRest = function (context, ...args) {
-    return (...calls) => this.apply(context, bindArgs.concat(calls));
+    return (...calls) => this.apply(context, args.concat(calls));
 }
+
+class Cat {
+    constructor(name) {
+        this.name = name;
+    }
+
+    says(sound, person) {
+        console.log(`${this.name} says ${sound} to ${person}!`);
+        return true;
+    }
+}
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBindRest(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBindRest(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBindRest(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBindRest(pavlov);
+notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
