61017bb0cbbeee07452951781458daa42ec6250a
atjohnfeng <atjohnfeng@gmail.com>
2021-09-21 15:14:03 -0700

    Test root

--- W12/W12D2/BenchBnB/frontend/bench_bnb.jsx
+++ W12/W12D2/BenchBnB/frontend/bench_bnb.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configureStore } from 'redux'
+// import { configureStore } from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    const store = configureStore();
-    
+    // const store = configureStore();
+
     ReactDOM.render(<h1>Hello</h1>, root);
 })
---
+++ W12/W12D2/BenchBnB/frontend/store/store.js
@@ -0,0 +1,9 @@
+// import { createStore, applyMiddleware } from "redux";
+// import rootReducer from "../reducers/root_reducer";
+// import myThunk from '../middleware/thunk';
+
+// const configureStore = (preloadedState = {}) => {
+//     return createStore(rootReducer, preloadedState, applyMiddleware(myThunk));
+// };
+
+// export default configureStore;
