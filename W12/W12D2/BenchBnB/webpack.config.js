23823fdf029e4980358e180fad21f8c121abda72
atjohnfeng <atjohnfeng@gmail.com>
2021-09-21 14:20:54 -0700

    Add webpack config

---
+++ W12/W12D2/BenchBnB/webpack.config.js
@@ -0,0 +1,28 @@
+
+var path = require('path');
+
+module.exports = {
+    entry: "./frontend/benchbnb.js",
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
