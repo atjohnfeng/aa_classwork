6b07189b775b58fceec8c615e73e04abaef776f3
atjohnfeng <atjohnfeng@gmail.com>
2021-09-21 17:24:01 -0700

    Add userscontroller methods

--- W12/W12D2/BenchBnB/app/controllers/api/users_controller.rb
+++ W12/W12D2/BenchBnB/app/controllers/api/users_controller.rb
@@ -10,6 +10,35 @@ class Api::UsersController < ApplicationController
         end
     end
 
+    def update
+        @user = User.find(params[:id])
+        if @user && @user.update(user_params)
+            render :show
+        elsif !@user
+            render json: ['User could not be found.'], status: 400
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        if @user
+            @user.destroy
+            render :show
+        else
+            render json: ['User could not be found.']
+        end
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
     private
 
     def user_params
